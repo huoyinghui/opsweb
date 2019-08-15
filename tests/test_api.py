152d04dd70659c273cd29ecd30e0bd349eef628d
huoyinghui <2285020853@qq.com>
2019-08-15 15:05:11 +0800

    add: tests

---
+++ tests/test_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author: 
+Email:
+
+date: 
+desc:
+
+"""
+
+
+def test_main():
+    assert 1 == 1
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
